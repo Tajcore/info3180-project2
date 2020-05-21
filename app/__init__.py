04a614aaa5e13871d5049d67bb70d0b405f48792
Tajcore <45900878+Tajcore@users.noreply.github.com>
2020-05-20 20:43:37 -0500

    requirements.txt update(missing module)

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "$up3rDup3R3K3Y"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://shaunjennings:@localhost/project2"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://vqvrwtdqkkstpp:c99e521893c023234f6ba7485406c4e435c79c840d551e2fce22e28a72ef05ed@ec2-35-171-31-33.compute-1.amazonaws.com:5432/d4q837gfeca0gi"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 app.config['UPLOAD_FOLDER'] = './app/static/uploads'
 app.config['GET_FILE'] = '/static/uploads'
